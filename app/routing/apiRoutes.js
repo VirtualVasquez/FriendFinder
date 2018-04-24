e4577de0c9abe1a3888be532d92cf6880d8166f4
MEGA-MAN-EXE\wolfe <mxv647@med.miami.edu>
2018-04-23 22:26:00 -0400

    moving in the right directions referencing solved hot restaurant

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,7 +1,29 @@
-//GET route witht he url `/api/friends`, which is used to display a JSON of all possible friends
-//POST route `/api/friends' to handle incoming survey results and compatiblity logic
+ // ===============================================================================
+// LOAD DATA
+// We are linking our routes to a series of "data" sources.
+// These data sources hold arrays of information on table-data, waitinglist, etc.
+// ===============================================================================
+
+var friendsData = require("../data/tableData");
+
+module.exports = function(app) {
+  // API GET Request
+  // Below code handles when users "visit" the survey page.
+  // In each of the below cases when a user visits a link
+  // they are shown a JSON of the data in the table
+  // ---------------------------------------------------------------------------	
 app.get("/api/friends", function(req, res) {
-  return res.send(friends);
+  res.send(friendsData);
+});
+
+// API POST Requests
+  // Below code handles when a user submits a form and thus submits data to the server.
+app.post("/api/friends", function(req, res) {
+	friendsData.push(req.body);
+  // var newFriend = req.body;
+  // console.log(req.body);
+  // addTable(newFriend.a1, newFriend.a2, newFriend.a3, newFriend.a4, newFriend.a5, newFriend.a6, newFriend.a7, newFriend.a8, newFriend.a9, newFriend.a10);
+  // res.json(newFriend);
 });
 
 function Friend(a1, a2, a3, a4, a5, a6, a7, a8, a9, a10){
--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,5 +1,19 @@
-//Need a GET route to `/survey` which should display the survey page
-//default, catch-call route that leads to `home.html`, which displays the home page
+// ===============================================================================
+// DEPENDENCIES
+// We need to include the path package to get the correct file path for our html
+// ===============================================================================
+var path = require("path");
+
+// ===============================================================================
+// ROUTING
+// ==============================================================================
+
+module.exports = function(app) {
+  // HTML GET Requests
+  // Below code handles when users "visit" a page.
+  // In each of the below cases the user is shown an HTML page of content
+  // ---------------------------------------------------------------------------
+
 app.get("/", function(req, res) {
 	console.log("path is: ",path.join(__dirname + "/../public/", "home.html"))
   res.sendFile(path.join(__dirname + "/../public/",  "home.html"));
@@ -10,7 +24,11 @@ app.get("/survey", function(req, res) {
   res.sendFile(path.join(__dirname + "/../public/",  "survey.html"));
 });
 
-app.get("/home", function(req, res) {
-	console.log("path is: ",path.join(__dirname + "/../public/", "home.html"))
-  res.sendFile(path.join(__dirname + "/../public/",  "home.html"));
-});
+  // If no matching route is found default to home
+  app.get("*", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/home.html"));
+  });
+};
+
+
+}
